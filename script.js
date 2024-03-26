516b351f55511a726a460d87f3b1eb71baaf8072
Gabriel Vinicius dos Santos <100848560+gabrielvinni@users.noreply.github.com>
2024-03-26 09:40:55 -0300

    criado a sessão - comandos básicos

---
+++ script.js
@@ -0,0 +1,11 @@
+function alerta() {
+    window.alert("Esse é um alerta")
+}
+
+function confirmar() {
+    window.confirm("Esse é o confirm, deseja continuar ?")
+}
+
+function escreva() {
+    window.prompt("Escreva se nome:")
+}
