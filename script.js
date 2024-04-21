b807fc6cce3cb6586e8209bb341802f29e329ecd
Gabriel Vinicius dos Santos <100848560+gabrielvinni@users.noreply.github.com>
2024-04-21 14:07:57 -0300

    n sei

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
+//   Botão abri e fecha das seções
+
 let h2 = document.querySelectorAll("h2")
 h2[0].addEventListener("click", abrirComandos)
 h2[1].addEventListener("click", abrirVariaveis)
-// -------------------------------------------------------
+
 function abrirComandos() {
     let com = document.querySelector("#comandos-basicos")
     com.classList.toggle("abrir")
@@ -11,15 +13,15 @@ function abrirVariaveis() {
     let vari = document.querySelector("#variaveis")
     vari.classList.toggle("abrir")
 }
-// -------------------------------------------------------
+
+// -------- Explos dos comandos --------------
+
 function alerta() {
     window.alert("Esse é um alerta")
 }
-
 function confirmar() {
     window.confirm("Esse é o confirm, deseja continuar ?")
 }
-
 function escreva() {
     window.prompt("Escreva se nome:")
 }
